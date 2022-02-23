2dce110c286b202c632c25388d291134e0c62ac5
Seyed Alireza Mousavi <am808036@gmail.com>
2022-02-23 19:03:19 +0330

    add buttonType to Button component and remove it's container <div>.

--- src/component/ButtonPanel.js
+++ src/component/ButtonPanel.js
@@ -1,40 +1,41 @@
 import {React} from 'react';
 
 import Button from './button';
+import './buttonpanel.css';
 
 export default function ButtonPanel () {
 
     return (
         <div className='component-button-panel'>
-            <div>
-                <Button name="AC" />
-                <Button name="+/-" />
-                <Button name="%" />
-                <Button name="÷" />
-            </div>
-            <div>
-                <Button name="7" />
-                <Button name="8" />
-                <Button name="9" />
-                <Button name="x" />
-            </div>
-            <div>
-                <Button name="4" />
-                <Button name="5" />
-                <Button name="6" />
-                <Button name="-" />
-            </div>
-            <div>
-                <Button name="1" />
-                <Button name="2" />
-                <Button name="3" />
-                <Button name="+" />
-            </div>
-            <div>
-                <Button name="0" />
-                <Button name="." />
-                <Button name="=" />
-            </div>
+            <>
+                <Button name="AC" buttonType="primary" />
+                <Button name="+/-" buttonType="primary" />
+                <Button name="%" buttonType="primary" />
+                <Button name="÷" buttonType="secondary" />
+            </>
+            <>
+                <Button name="7" buttonType="primary"/>
+                <Button name="8" buttonType="primary"/>
+                <Button name="9" buttonType="primary"/>
+                <Button name="x" buttonType="secondary"/>
+            </>
+            <>
+                <Button name="4" buttonType="primary"/>
+                <Button name="5" buttonType="primary"/>
+                <Button name="6" buttonType="primary"/>
+                <Button name="-" buttonType="secondary"/>
+            </>
+            <>
+                <Button name="1" buttonType="primary" />
+                <Button name="2" buttonType="primary" />
+                <Button name="3" buttonType="primary" />
+                <Button name="+" buttonType="secondary" />
+            </>
+            <>
+                <Button name="0" buttonType="primary" />
+                <Button name="." buttonType="primary" />
+                <Button name="=" buttonType="secondary" />
+            </>
         </div>
     );
 }
