69d05a230c34f46f67b734515940bce850e984da
Seyed Alireza Mousavi <am808036@gmail.com>
2022-02-26 00:49:56 +0330

    add Display component for showing result of calculation, and style of it.

---
+++ src/component/Display.js
@@ -0,0 +1,9 @@
+import "./display.css";
+
+export default function Display () {
+  return (
+    <div className="component-display">
+      <p className="display-text">11 + 2</p>
+    </div>
+  );
+}
