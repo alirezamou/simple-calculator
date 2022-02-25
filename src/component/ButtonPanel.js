2586682fcd82c7395ee7dfa4c3a3815512089983
Seyed Alireza Mousavi <am808036@gmail.com>
2022-02-26 00:50:49 +0330

    change empty tags to React.Fragment tag.

--- src/component/ButtonPanel.js
+++ src/component/ButtonPanel.js
@@ -7,35 +7,35 @@ export default function ButtonPanel () {
 
     return (
         <div className='component-button-panel'>
-            <>
+            <React.Fragment>
                 <Button name="AC" buttonType="primary" />
                 <Button name="+/-" buttonType="primary" />
                 <Button name="%" buttonType="primary" />
                 <Button name="÷" buttonType="secondary" />
-            </>
-            <>
+            </React.Fragment>
+            <React.Fragment>
                 <Button name="7" buttonType="primary"/>
                 <Button name="8" buttonType="primary"/>
                 <Button name="9" buttonType="primary"/>
                 <Button name="x" buttonType="secondary"/>
-            </>
-            <>
+            </React.Fragment>
+            <React.Fragment>
                 <Button name="4" buttonType="primary"/>
                 <Button name="5" buttonType="primary"/>
                 <Button name="6" buttonType="primary"/>
                 <Button name="-" buttonType="secondary"/>
-            </>
-            <>
+            </React.Fragment>
+            <React.Fragment>
                 <Button name="1" buttonType="primary" />
                 <Button name="2" buttonType="primary" />
                 <Button name="3" buttonType="primary" />
                 <Button name="+" buttonType="secondary" />
-            </>
-            <>
+            </React.Fragment>
+            <React.Fragment>
                 <Button name="0" buttonType="primary" />
                 <Button name="." buttonType="primary" />
                 <Button name="=" buttonType="secondary" />
-            </>
+            </React.Fragment>
         </div>
     );
 }
